694c0aae07d7377c44799fe228da3b83e6149468
ssongman <ssongmantop@gamil.com>
2018-08-26 21:53:37 +0900

    World__ 변경

--- hello-server/src/main/java/io/pivotal/helloserver/HelloController.java
+++ hello-server/src/main/java/io/pivotal/helloserver/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     String greeting;
 
     @RequestMapping("/hello")
-    public String sayHello(@RequestParam(value="name", defaultValue="World_") String toWho) {
+    public String sayHello(@RequestParam(value="name", defaultValue="World__") String toWho) {
         String result = String.format(greeting, toWho);
         System.out.println(result);
         return result;
